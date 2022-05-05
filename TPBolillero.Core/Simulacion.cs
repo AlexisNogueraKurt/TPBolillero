6a108ffdac08b02f8d16e78c65eb6546a480060b
Alexis Noguera <ale.capo.9634@gmail.com>
2022-05-05 15:30:16 -0300

    Simulacion

---
+++ TPBolillero.Core/Simulacion.cs
@@ -0,0 +1,15 @@
+namespace TPBolillero.Core
+{
+    public class Simulacion
+    {
+        public int SimularSinHilos()
+        {
+
+        }
+
+        public int SimularConHilos()
+        {
+        
+        }
+    }
+}
